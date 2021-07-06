837585221890305def0609f2dd100eb68a184e31
bonbol-centerpoint <cafeku.centerpoint@gmail.com>
2021-07-06 16:48:15 +0800

    perbaiki timer launching

--- utils/launching.js
+++ utils/launching.js
@@ -1,14 +1,17 @@
 function hitungHari() {
-    const tanggalTujuan = new Date('July 7, 2021 08:00:00').getTime();
+    const tanggalTujuan = new Date('July 7, 2021 07:07:07').getTime();
     
     const sekarang = new Date().getTime();
     const selisih = tanggalTujuan - sekarang;
-    const hari = Math.floor(selisih / (1000 * 60 * 60 * 24));
-    const jam = Math.floor( selisih % (1000 * 60 * 60* 24)/(1000*60*60));
-    const menit = Math.floor( selisih % (1000 * 60 * 60) / (1000*60));
+    let hari = Math.floor(selisih / (1000 * 60 * 60 * 24));
+    let jam = Math.floor( selisih % (1000 * 60 * 60* 24)/(1000*60*60));
+    let menit = Math.floor( selisih % (1000 * 60 * 60) / (1000*60));
     const detik = Math.floor(selisih % (1000 * 60) / 1000);
-    const result = hari + " HARI " + jam + " JAM " + menit + " MENIT ";
-    return result;
+    if ( jam + menit + detik <= 0 ) {
+        return "Launching Today!";
+    } else {
+        return `Launching dalam ${jam} Jam + ${menit} Menit lagi`;
+    }
     // return hitungMundur;
     // return result;
 } 
